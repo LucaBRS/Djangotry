19eb591b067fb290d2effdaa66e5b4b3138e45d0
Luca Barsottini <luca.barsottini@mail.polimi.it>
2020-06-07 23:17:07 +0200

    migrations

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Article(models.Model):
+    #questo è un modello e bisogna dare la MIGRAZIONE
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add thumbani
+    # autore
+
+
+#### how to do migrations   ####
+    # manage.py makemigration
+    # manage.py migrate
