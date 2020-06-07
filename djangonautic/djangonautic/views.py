be683082f6858a332bb4d4ebd0e49aa405846f0c
Luca Barsottini <luca.barsottini@mail.polimi.it>
2020-06-07 21:11:13 +0200

    TheNetNinja

---
+++ djangonautic/djangonautic/views.py
@@ -0,0 +1,20 @@
+#prima cosa è importare
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+###!!!! andare dentro settings e cercare per TEMPLATES nella sezione DIRS mettere la 'directory' delle pagine 
+
+def homepage(request):
+   # return HttpResponse('homepage')
+   #retorn template e importiamo il modulo per reder
+   return render(request,'homepage.html')
+
+def about(request):
+    #return HttpResponse('about')
+     #retorn template e importiamo il modulo per reder
+    return render(request,'about.html')
+
+def project(request);
+    return render(request,'index.html')
